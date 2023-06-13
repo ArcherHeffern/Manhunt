6c9c1e2bbb8b58f2bf1fd9f060db032eef46d606
ArcherHeffern <heffernarcher@gmail.com>
2023-06-13 00:07:08 -0400

    ejected or something

--- App.tsx
+++ App.tsx
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './pages/home';
 import CreateGame from './pages/createGame';
 import WaitingQueue from './pages/waitingQueue';
 import Game from './pages/game';
 import HowToPlay from './pages/howToPlay';
-import { createClient } from '@supabase/supabase-js'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 const Stack = createNativeStackNavigator();
 
 
-const supabaseUrl = 'https://loujtodxdvzommyauzdb.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 export default function App() {
 
-  const [userName, setUserName] = useState('');
+  const [user, setUser] = useState<FirebaseAuthTypes.User|null>(null);
 
+  const [initializing, setInitializing] = useState(true);
+
+  auth().onAuthStateChanged((user) => {
+    console.log(`User: ${user}`);
+    if (user) {
+      // user logged in
+      setUser(user);
+    } else {
+      // user logged out
+    }
+  });
+
+  auth().signInAnonymously();
+
+  if (initializing) return null;
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="CreateGame" component={CreateGame} />
         <Stack.Screen name="WaitingQueue" component={WaitingQueue} />
         <Stack.Screen name="Game" component={Game} />
-        <Stack.Screen name="HowToPlay" component={HowToPlay}/>
+        <Stack.Screen name="HowToPlay" component={HowToPlay} />
       </Stack.Navigator>
     </NavigationContainer>
   );
